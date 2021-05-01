079c50e139d136e0d52e8fb4b97c0e0bd9309028
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-01 17:14:47 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma(phrase)
     "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase == phrase.upcase 
+  else
     return "NO, NOT SINCE 1938!"
   end
 end
